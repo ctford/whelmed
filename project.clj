dd2116598bc9412ada63b1f1a48cebf1010d9504
Chris Ford <christophertford@gmail.com>
2012-11-20 21:02:18 +0530

    Upgrade Clojure and Leipzig.

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject whelmed "0.1.0-SNAPSHOT"
   :description "Whelmed. Not overwhelmed. Not underwhelmed. Just whelmed."
+  :url "http://github.com/ctford/whelmed"
   :main ^{:skip-aot true} whelmed.core
   :dependencies	[
-    [org.clojure/clojure "1.3.0"]
+    [org.clojure/clojure "1.4.0"]
     [overtone "0.7.1"]
-    [leipzig "0.1.0-SNAPSHOT"]
+    [leipzig "0.1.0"]
     [org.clojure/math.numeric-tower "0.0.1"]])
