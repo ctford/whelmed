793a35b77ddb6cdf3d6e99ba1abb4ddd76125daa
Chris Ford <christophertford@gmail.com>
2012-11-21 21:28:31 +0530

    Remove dependency on seesaw so that we don't get warnings.

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :main ^{:skip-aot true} whelmed.core
   :dependencies	[
     [org.clojure/clojure "1.4.0"]
-    [overtone "0.7.1"]
-    [leipzig "0.1.0"]
+    [overtone "0.7.1" :exclusions [seesaw]]
+    [leipzig "0.1.0" :exclusions [seesaw]]
     [org.clojure/math.numeric-tower "0.0.1"]])
