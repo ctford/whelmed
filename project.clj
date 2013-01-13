8985e257989fc222d5b7c431bd9e6e47a9af5b1c
Chris Ford <christophertford@gmail.com>
2013-01-13 11:41:08 +0530

    Upgrade to Leipzig 0.2.

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
   :dependencies	[
     [org.clojure/clojure "1.4.0"]
     [overtone "0.8.0-RC5"]
-    [leipzig "0.2.0-SNAPSHOT" :exclusions [seesaw]]
+    [leipzig "0.2.0" :exclusions [seesaw]]
     [org.clojure/math.numeric-tower "0.0.1"]])
