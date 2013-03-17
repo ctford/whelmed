902c539638654452fe1ec6f6f4b19e82e43d3004
Chris Ford <christophertford@gmail.com>
2013-03-17 18:23:18 +0300

    Bassline

--- src/whelmed/songs/in_the_mood.clj
+++ src/whelmed/songs/in_the_mood.clj
@@ -41,27 +41,30 @@
           (phrase (cycle [1/2]) (cycle [2 4 7]))
           (take 11)
           (then (phrase [5/2] [7]))
-          (with (phrase [7 1] [0 0]))
           (where :part (is ::melody))) 
-        extra (->>
-          (phrase (repeat 1) [0 1 2 3 4 3 2 1])
-          (where :pitch #(- % 7)) 
-          (where :part (is ::harmony))) 
+        bassline (fn [root] (->>
+          (phrase [1 1 1 1/2 1/2 1 1 1 1] [0 2 4 5 4 7 5 4 2])
+          (where :pitch #(+ % root))
+          (where :pitch (comp low low low))
+          (where :part (is ::bassline)))) 
         beat (->>
           (tap :tick [1 3 5 7] 8)
           (with (tap :kick [0 1/2 3/2 5/2 7/2 8/2 10/2 11/2 13/2] 8))
           (where :part (is ::beat)))
-        chords (->>
-          (phrase (cycle [7 1]) [0 0 0 0 3 3 0 0 -3 -3 0 0])
+        chords (fn [root] (->>
+          (phrase (cycle [7 1]) (repeat root))
           (where :pitch low)
           (canon (comp (interval 2) (partial canon (interval 2))))
           (canon (interval 7))
-          (where :part (is ::chords)))]
+          (where :part (is ::chords))))]
   (->>
     (->> melody (times 4) (then (->> melody (where :pitch inc))) (then melody))
+    (with (->> (map bassline [0 0 3 0 4 0]) (reduce #(then %2 %1))))
+    (with (->> (map chords [0 0 3 0 4 0]) (reduce #(then %2 %1))))
     (with chords)
-    (with (->> beat (with extra) (times 6)))
+    (with (->> beat (times 6)))
     swing
+;    (filter (comp #(= % ::bassline) :part))
     (in-time (bpm 180))
     (wherever :pitch, :pitch (comp G major)))))
 
@@ -69,14 +72,14 @@
           :tick drums/closed-hat,
           :tock drums/open-hat})
 
-(defmethod play-note ::harmony [note] (pick 0.90 0.3 note))
-(defmethod play-note ::melody [{midi :pitch}] (sampled-piano midi))
+(defmethod play-note ::bassline [note] (pick 0.40 0.3 note))
+(defmethod play-note ::melody [note] (pick 0.90 0.3 note))
+(defmethod play-note ::chords [{midi :pitch}] (sampled-piano midi 0.5))
 (defmethod play-note ::beat [note] ((-> note :drum kit)))
-(defmethod play-note ::chords [{midi :pitch, length :duration}]
+(defmethod play-note ::melody [{midi :pitch, length :duration}]
   (organ-cornet (overtone/midi->hz midi) length 0.1))
 
 (comment
   (def riff nil) 
   (jam riff)
-  (jam beated)
 ) 
