796ee6a92e3731c7284804c6ebe7160775d9874e
Chris Ford <christophertford@gmail.com>
2013-03-17 20:25:06 +0300

    Factor out the progression.

--- src/whelmed/songs/in_the_mood.clj
+++ src/whelmed/songs/in_the_mood.clj
@@ -37,7 +37,8 @@
   (map #(zipmap [:time :duration :drum] [%1 (- length %1) drum]) times))
 
 (def riff
-  (let [melody (->>
+  (let [progression [0 0 3 0 4 0]
+        melody (->>
           (phrase (cycle [1/2]) (cycle [2 4 7]))
           (take 11)
           (then (phrase [5/2] [7]))
@@ -52,32 +53,30 @@
           (with (tap :kick [0 1/2 3/2 5/2 7/2 8/2 10/2 11/2 13/2] 8))
           (where :part (is ::beat)))
         chords (fn [root] (->>
-          (phrase (cycle [7 1]) (repeat root))
-          (where :pitch low)
-          (canon (comp (interval 2) (partial canon (interval 2))))
-          (canon (interval 7))
+          (phrase [7 1] (repeat root)) 
+          (where :pitch low) 
+          (canon (comp (interval 2) (partial canon (interval 2)))) 
           (where :part (is ::chords))))]
   (->>
     (->> melody (times 4) (then (->> melody (where :pitch inc))) (then melody))
-    (with (->> (map bassline [0 0 3 0 4 0]) (reduce #(then %2 %1))))
-    (with (->> (map chords [0 0 3 0 4 0]) (reduce #(then %2 %1))))
-    (with chords)
+    (with (->> (map bassline progression) (reduce #(then %2 %1))))
+    (with (->> (map chords progression) (reduce #(then %2 %1))))
     (with (->> beat (times 6)))
     swing
-;    (filter (comp #(= % ::bassline) :part))
+;    (filter (comp #(= % ::chords) :part))
     (in-time (bpm 180))
     (wherever :pitch, :pitch (comp G major)))))
 
 (def kit {:kick drums/kick2
-          :tick drums/closed-hat,
+          :tick drums/closed-hat
           :tock drums/open-hat})
 
 (defmethod play-note ::bassline [note] (pick 0.40 0.3 note))
 (defmethod play-note ::melody [note] (pick 0.90 0.3 note))
 (defmethod play-note ::chords [{midi :pitch}] (sampled-piano midi 0.5))
 (defmethod play-note ::beat [note] ((-> note :drum kit)))
-(defmethod play-note ::melody [{midi :pitch, length :duration}]
-  (organ-cornet (overtone/midi->hz midi) length 0.1))
+;(defmethod play-note ::melody [{midi :pitch, length :duration}]
+;  (organ-cornet (overtone/midi->hz midi) length 0.1))
 
 (comment
   (def riff nil) 
