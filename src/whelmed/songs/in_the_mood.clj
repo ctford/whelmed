c0ec52ce175c4e6a7c10365d905fc8adc86a787c
Chris Ford <christophertford@gmail.com>
2013-03-23 21:00:17 +0300

    More simplification, including removing canon.

--- src/whelmed/songs/in_the_mood.clj
+++ src/whelmed/songs/in_the_mood.clj
@@ -2,13 +2,12 @@
   (:use
     [leipzig.melody]
     [whelmed.melody]
-    [leipzig.scale] 
-    [leipzig.canon])
+    [leipzig.scale])
   (:require
     [overtone.live :as overtone]
     [overtone.inst.drum :as drums]))
 
-(defn- mapthen [f coll] (->> coll (map f) (reduce #(then %2 %1))))
+(defn mapthen [f notes] (->> notes (map f) (reduce #(then %2 %1))))
 (defn swing [beat] ((scale [2/3 1/3]) (* 2 beat)))
 
 (defn tap [drum times length]
@@ -17,31 +16,32 @@
 (def riff
   (let [progression [0 0 3 0 4 0]
         melody (->>
-          (phrase (cycle [1/2]) (cycle [2 4 7]))
-          (take 11)
-          (then (phrase [5/2] [7]))
+          (take 11 (phrase (repeat 1/2) (cycle [-5 -3 0]))) 
+          (then (phrase [5/2] [0]))
           (where :part (is ::melody))) 
         bassline (fn [root] (->>
           (phrase (repeat 1) [0 2 4 5 7 5 4 2])
-          (where :pitch #(+ % root))
+          (where :pitch (from root))
           (where :pitch (comp low low low))
           (where :part (is ::bassline)))) 
         beat (->>
           (tap :tick [1 3 5 7] 8)
           (with (tap :kick [0 1/2 3/2 5/2 7/2 8/2 10/2 11/2 13/2] 8))
           (where :part (is ::beat)))
-        chords (fn [root] (->>
-          (phrase [7 1] (repeat root)) 
-          (where :pitch low) 
-          (canon (comp (interval 2) (partial canon (interval 2)))) 
-          (where :part (is ::chords))))]
+        chords (fn [root]
+          (let [bass (phrase (cycle [7 1]) (repeat root))]
+            (->> bass
+              (with ((interval 2) bass)) 
+              (with ((interval 4) bass)) 
+              (where :part (is ::chords)))))]
   (->>
     (->> melody (times 4) (then (->> melody (where :pitch inc))) (then melody))
     (with (mapthen bassline progression))
     (with (mapthen chords progression))
     (where :pitch (comp G major))
     (with (->> beat (times 6)))
-    (in-time (comp (bpm 180) swing)))))
+    (in-time swing)
+    (in-time (bpm 180)))))
 
 (def kit {:kick drums/kick2
           :tick drums/closed-hat
