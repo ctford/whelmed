079b40dc9fc250d7be5a339328c1e1087d10bb59
Chris Ford <christophertford@gmail.com>
2013-03-23 15:13:26 +0300

    Use scale to define lilt.

--- src/whelmed/songs/in_the_mood.clj
+++ src/whelmed/songs/in_the_mood.clj
@@ -13,12 +13,7 @@
     [overtone.inst.drum :as drums]
     [overtone.synth.stringed :as strings])) 
 
-(defn lilt [time]
-  (let [whole-part (math/floor time)
-        remainder (- time whole-part)]
-    (if (<= remainder 1/2)
-      (+ whole-part (* 4/3 remainder))
-      (+ whole-part (* 2/3 remainder)))))
+(defn lilt [beat] ((scale [2/3 1/3]) (* 2 beat)))
 
 (defn swing [notes]
   (map
@@ -53,9 +48,11 @@
           (with (tap :kick [0 1/2 3/2 5/2 7/2 8/2 10/2 11/2 13/2] 8))
           (where :part (is ::beat)))
         chords (fn [root] (->>
-          (phrase [7 1] (repeat root)) 
+          ;(phrase [7 1] (repeat root)) 
+          (phrase (cycle [1/4 1/4 1/2]) (cycle [root (+ root 2) (+ root 4)])) 
+          (take 24)
           (where :pitch low) 
-          (canon (comp (interval 2) (partial canon (interval 2)))) 
+          ;(canon (comp (interval 2) (partial canon (interval 2)))) 
           (where :part (is ::chords))))]
   (->>
     (->> melody (times 4) (then (->> melody (where :pitch inc))) (then melody))
@@ -63,7 +60,7 @@
     (with (->> (map chords progression) (reduce #(then %2 %1))))
     (with (->> beat (times 6)))
     swing
-    ;(filter (comp #(= % ::beat) :part))
+    ;(filter (comp #(= % ::chords) :part))
     (in-time (bpm 180))
     (wherever :pitch, :pitch (comp G major)))))
 
@@ -78,8 +75,8 @@
     (overtone/at (+ start duration) (overtone/ctl id :gate 0))
                                                 ))
 (defmethod play-note ::beat [note] ((-> note :drum kit)))
-;(defmethod play-note ::melody [{midi :pitch, length :duration}]
-;  (organ-cornet (overtone/midi->hz midi) length 0.1))
+;(defmethod play-note ::chords [{midi :pitch, length :duration}]
+;  (organ-cornet (overtone/midi->hz midi) length))
 
 (comment
   (def riff nil) 
