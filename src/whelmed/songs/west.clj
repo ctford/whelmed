c1f6b28e77826a7347f1c75d1a3f78556347ae21
Chris Ford <christophertford@gmail.com>
2015-04-15 11:24:08 +0100

    Refactor how the melodies fit together.

--- src/whelmed/songs/west.clj
+++ src/whelmed/songs/west.clj
@@ -17,60 +17,54 @@
 
 ; Accompaniment
 (def backing
-  (let [render-chord (fn [[left? chord]] (->> (phrase [4] [chord]) (all :left? left?)))]
+  (let [lefts [true false true false]
+        render-chord (fn [[left? chord]] (->> (phrase [4] [chord]) (all :left? left?)))]
     (->>
       progression
-      (map vector [true false true false])
+      (map vector lefts)
       (mapthen render-chord)
       (all :part ::accompaniment))))
+
+(defn vary [f notes]
+  (->> notes (then (f notes))))
 
 ; Lead
 (def ill-run-away
   (->>
-   (phrase
-     [1/2 1/4 1/4 1/2]
-     [  3   4   3   4])
-  (after -1/2)))
-
-(def ill-get-away
-  (->> ill-run-away
-    (wherever (between? 1/4 1/2), :pitch (from 3))))
+    (after -1/2
+           (phrase
+             [1/2 1/4 1/4 1/2 3]
+             [  3   4   3   4 nil]))
+    (vary (partial but 1/4 1/2 (phrase [1/4] [6])))))
 
 (def my-heart-will-go-west-with-the-sun
-  (after -1/2
-     (phrase
-       [1/2 3/4 3/4 2/4 3/4 3/4 1/4 17/4]
-       [  3   4   3   2   4   3   2   -1])))
+  (->> (phrase
+         [1/2 3/4 3/4 2/4 3/4 3/4 1/4 17/4]
+         [  3   4   3   2   4   3   2   -1])
+       (after -1/2)))
 
 (def west-with-the-west-with-the 
-  (let [west-with-the
-          (->> my-heart-will-go-west-with-the-sun
-               (take 4) (drop 1) (times 4))]
-  (->>
-    [{:time -1/2 :pitch 3 :duration 1/2}]
-    (then west-with-the))))
+  (->> (after -1/2 (phrase [1/2] [3]))
+       (then (times 4 (phrase [3/4 3/4 2/4] [4 3 2])))))
 
 (def theme
   (->>
     ill-run-away
-    (then (after 3 ill-get-away))
-    (then (after 3 my-heart-will-go-west-with-the-sun))
+    (then my-heart-will-go-west-with-the-sun)
     (all :part ::lead)))
-
-(def half-theme
-  (let [crippled-theme
-          (->> ill-run-away
-               (then (->> ill-get-away (after 3))))]
-    (->>
-      (after 1/2 crippled-theme)
-      (then (after 3 crippled-theme))
-      (all :part ::lead))))
 
 (def spilling-theme
   (->>
     ill-run-away
-    (then (after 3 ill-get-away))
-    (then (after 3 west-with-the-west-with-the))
+    (then west-with-the-west-with-the)
+    (all :part ::lead)))
+
+(def shifted-theme
+  (->>
+    ill-run-away
+    (times 2)
+    (after 1/2)
+    drop-last
     (all :part ::lead)))
 
 ; Response
@@ -80,8 +74,7 @@
     [  4   3   4   6    4]))
 
 (def like-fairy-floss
-  (with [{:time -1/4 :pitch 3 :duration 1/4}]
-        a-parting-kiss))
+  (with (after -1/4 (phrase [1/4] [3])) a-parting-kiss))
 
 (def dissolves-on-the-tip-of-my-tongue
   (->>
@@ -94,8 +87,7 @@
  (->>
    a-parting-kiss
    (then like-fairy-floss)
-   (then dissolves-on-the-tip-of-my-tongue) 
-   (then dissolves-on-the-tip-of-my-tongue)
+   (then (times 2 dissolves-on-the-tip-of-my-tongue)) 
    (all :part ::response)))
 
 ; Break
@@ -107,27 +99,22 @@
 
 (def consider-that
   (->> consider-this
-    (wherever (between? 0 1/2), :pitch dec)))
+       (but 0 1/2 (phrase [8/2] [6]))))
 
 (def consider-everything
-  (->>
-    (take 3 consider-this)
-    (then
-      (phrase
-        [2/2 1/2 2/2 2/2 9/2]
-        [  7   8   7   6   4]))))
+  (->> consider-this
+       (but 0 8
+            (phrase
+              [2/2 1/2 2/2 2/2 9/2]
+              [  7   8   7   6   4]))))
 
-(def breakdown
+(def break
  (->>
    consider-this
    (then consider-that)
-   (then consider-everything)))
-
-(def breakup (->> breakdown (where :pitch lower)))
-(def break
-  (->>
-    (with breakup breakdown)
-    (all :part ::break)))
+   (then consider-everything)
+   (canon/canon (canon/interval -7))
+   (all :part ::break)))
 
 ; Bass
 (def light-bass
@@ -147,7 +134,7 @@
 
 (def beat
   (->> (times 4 (phrase [6/4 4/4 6/4] (repeat -14)))
-       (with (->> (phrase [1 2 2 2 1/2 1/2] (cons nil (repeat -10))) (times 2)))
+       (with (times 2 (phrase [1 2 2 2 1/2 1/2] (cons nil (repeat -10)))))
        (all :part ::kick)))
 
 (def flat-beat
@@ -176,7 +163,7 @@
           (->> theme (then spilling-theme)
             (with (->> (with beat accompaniment) (times 2))))
         fadeout
-          (->> accompaniment (with half-theme beat) (then bass))]
+          (->> accompaniment (with shifted-theme beat) (then bass))]
     (->>
       intro (then call) (then response)
       (then (->> break (with light-bass flat-beat) (times 2)
