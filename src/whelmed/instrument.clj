e5c648b4cda0f8c3e28b7d1f3d55a06f489b82f2
Chris Ford <christophertford@gmail.com>
2015-04-16 21:33:02 +0100

    Give Sidhe the 'War of the worlds' treatment.

--- src/whelmed/instrument.clj
+++ src/whelmed/instrument.clj
@@ -4,18 +4,24 @@
     [overtone.live])
   (:require [overtone.synth.stringed :as strings])) 
 
-(defsynth shudder [freq 440 vibrato 6 pan 0 wet 0.5 volume 1.0]
+(defcgen cut-out [input {:default :none}]
+  (:kr (do (detect-silence input :action FREE)
+           input))
+  (:default :kr))
+
+
+(defsynth shudder [freq 440 vibrato 6 pan 0 wet 0.5 volume 1.0 room 0.5]
   (out 0
        (-> (square freq)
            (* (sin-osc freq))
            (* (env-gen (perc 2 1.5) :action FREE))
            (* (sin-osc vibrato))
            (* 2/3 volume)
-           (free-verb :mix wet)
+           (free-verb :mix wet :room room)
            (pan2 (line:ar pan (- pan) 3.5)))))
 
-(defsynth sawish [freq 440 duration 1.5 vibrato 8/3 depth 1 volume 1.0 pan 0.0 wet 0.5]
-  (let [envelope (env-gen (perc 0.01 duration) :action FREE)]
+(defsynth sawish [freq 440 duration 1.5 vibrato 8/3 depth 1 volume 1.0 pan 0.0 wet 0.5 room 0.5]
+  (let [envelope (env-gen (perc 0.01 duration))]
     (out 0
          (-> (sin-osc (* freq 0.51))
              (+ (* 3 (sin-osc freq)))
@@ -23,10 +29,11 @@
              (* 2)
              (* 2/3 volume envelope)
              (rlpf (mul-add (sin-osc vibrato) (* freq depth) (* 2 freq)) 1/3)
-             (free-verb :mix wet)
-             (pan2 pan)))))
+             (free-verb :mix wet :room room)
+             (pan2 pan)
+             cut-out))))
 
-(defsynth groan [freq 440 duration 10 vibrato 8/3 volume 1.0 position 0 wet 0.5 low 0.25]
+(defsynth groan [freq 440 duration 10 vibrato 8/3 volume 1.0 position 0 wet 0.5 room 0.5 low 0.25]
   (let [envelope (* (sin-osc vibrato)
                     (env-gen (perc 0.1 duration) :action FREE))]
     (out 0
@@ -36,10 +43,10 @@
               (* (sin-osc 0.8) (+ -0.03 (square freq)))
               (+ -0.04 (sin-osc freq)))
              (* 0.7 volume envelope)
-             (free-verb :mix wet)
+             (free-verb :mix wet :room room)
              (pan2 position)))))
 
-(defsynth bell [frequency 440 duration 1.0 volume 1.0 position 0 wet 0.5
+(defsynth bell [frequency 440 duration 1.0 volume 1.0 position 0 wet 0.5 room 0.5
                 h0 1 h1 0.6 h2 0.4 h3 0.25 h4 0.2 h5 0.15]
   (let [harmonics   [ 1  2  3  4.2  5.4 6.8]
         proportions [h0 h1 h2   h3   h4  h5]
@@ -68,19 +75,22 @@
          (ektara midi :distort distort :amp amp :gate 1))]
     (at (+ start (* 1000 duration)) (ctl synth-id :gate 0))))
 
-(definst brassy [freq 440 dur 1.0 vol 1 growl 1]
-  (let [cutoff (line:kr 1.0 0.0 dur)]
-    (-> (+
-         (* (sin-osc freq)
-            (env-gen (adsr 0.15 0.3 0.3) cutoff :action FREE))
-         (* 1/3 (sin-osc (* 3.01 freq))
-            (env-gen (adsr 0.3 0.3 0.2) cutoff))
-         (* 1/4 (sin-osc (* 5.03 freq))
-            (env-gen (adsr 0.3 0.3 0.2) cutoff))
-         (* 1/7 (sin-osc (* 7.04 freq))
-            (env-gen (adsr 0.4 0.3 0.2) cutoff)))
-        (* vol)
-        (lpf (+ (* 5 freq) (* (line:kr (* growl 6) 1 0.1) freq (sin-osc 50)))))))
+(defsynth brassy [freq 440 dur 1.0 vol 1 growl 1 noise 1.0 position 0.0]
+  (let [cutoff (line:kr 1.0 0.0 dur)
+        whole (-> (+
+                   (* (sin-osc freq) (env-gen (adsr 0.0 0.3 0.3) cutoff))
+                   (* 1/3 (sin-osc (* 2.01 freq)) (env-gen (adsr 0.15 0.2 0.4) cutoff))
+                   (-> (brown-noise) (* 1/2 noise) (* (env-gen (perc 0.0 0.1))))
+                   (* 1/2 (sin-osc (* 2 freq)) (env-gen (adsr 0.1 0.3 0.2) (line:kr 1.0 0.0 dur))))
+                  (* vol 5)
+                  (clip2 0.5)
+                  (* 3)
+                  (lpf (+ (* 3 freq) (* (line:kr (* growl 2) 1 0.1) freq (sin-osc 40))))
+                  (lpf 1500)
+                  (pan2 position)
+                  (free-verb :room 0.5 :mix 0.6))]
+    (detect-silence whole :action FREE)
+    (out 0 whole)))
 
 (definst woah [freq 440 duration 1 volume 1.0]
   (let [fenv (* (env-gen (perc 0.1 duration)) freq)
@@ -91,26 +101,52 @@
   (let [envelope (env-gen (perc 0.05 0.2) :action FREE)]
     (* volume envelope (pulse 5000 100))))
 
-(defsynth organ [freq 440 dur 1.0 vol 1.0 pan 0.0 wet 0.5]
+(defsynth organ [freq 440 dur 1.0 vol 1.0 pan 0.0 wet 0.5 room 0.5]
   (out 0
        (->
          (map #(sin-osc (* freq %)) (range 1 5))
          mix
-         (free-verb :mix wet)
+         (free-verb :mix wet :room room)
          (pan2 pan)
          (* vol)
-         (* (env-gen (asr 0.1 1.0 0.5)
-              (line:kr 1.0 0.0 dur)
-              :action FREE))
+         (* (env-gen (asr 0.1 1.0 0.5) (line:kr 1.0 0.0 dur) :action FREE))
          (lpf (mul-add (sin-osc 5) freq (* freq 5))))))
 
-(definst kraft-bass [freq 440 dur 1.0 vol 1.0]
-  (let [envelope (env-gen (asr 0 1 1)
-                          (line:kr 1.0 0.0 dur)
-                          :action FREE)
-        level (+ 100 (env-gen (perc 0 3)
-                              :level-scale 6000))
+(defonce random-walk (audio-bus))
+
+(defsynth walker [out-bus 0 freq 0.5]
+  (out:kr out-bus (mul-add (lf-noise1:kr freq) 1500 2000)))
+
+(defonce main (group "main"))
+(defonce inputs (group "inputs" :head main))
+(defonce outputs (group "latecomers" :after inputs))
+
+(defonce walk (walker [:tail inputs] random-walk))
+
+(defsynth corgan [freq 440 dur 1.0 vol 1.0 pan 0.0 wet 0.5 room 0.5 vibrato 3]
+  (out 0
+       (->
+         (saw freq)
+         (* 99)
+         (rlpf (mul-add (sin-osc vibrato) (line:kr 0 (in:kr random-walk) 10) (* freq 4)) 1/20)
+         (clip2 0.4)
+         (* vol)
+         (* (env-gen (asr 0.01 1.0 0.5) (line:kr 1.0 0.0 dur)))
+         (+ (* 1/4 (sin-osc (* 1.002 freq)) (env-gen (perc 0.3 dur))))
+         (rlpf (in:kr random-walk) 1/5)
+         (free-verb :mix wet :room 0.5)
+         (pan2 pan)
+         cut-out)))
+
+(defsynth kraft-bass [freq 440 dur 1.0 vol 1.0 pan 0 wet 0.5 room 0.5]
+  (let [envelope (env-gen (asr 0 1 1) (line:kr 1.0 0.0 dur))
+        level (+ 100 (env-gen (perc 0 3) :level-scale 6000))
         osc (mix [(saw freq)
                   (saw (* freq 1.005))
                   (pulse (/ freq 2) 0.5)])]
-    (-> osc (lpf level) (* vol envelope))))
+    (out 0 (-> osc
+               (lpf level)
+               (free-verb :room room :mix wet)
+               (pan2 pan)
+               (* vol envelope)
+               cut-out))))
--- src/whelmed/songs/sidhe.clj
+++ src/whelmed/songs/sidhe.clj
@@ -10,46 +10,44 @@
   (:require [overtone.live :as overtone]
             [overtone.inst.drum :as drums]
             [overtone.synth.stringed :as strings]))
-(def beat 
-  (->>
-    [(->> (rhythm [3/2 1/2 1/2 3/2]) (where :drum (is :kick)))
-     (->> (rhythm [2 1]) (after 1) (where :drum (is :tock)))
-     (->> (rhythm [1/2]) (after 7/2) (where :drum (is :tick)))]
-    (reduce with)
-    (times 8)
-    (all :part ::beat)))
 
+
+; First section
 (def bassline
-  (->>
-    (phrase (repeat 4) (range 0 -8 -1))
-    (where :pitch lower)
-    (all :part ::bass)))
+  (->> (phrase (repeat 4) (range 0 -8 -1))
+       (where :pitch lower)))
 
-(def intro
-  (let [vectorise (fn [chord] (-> chord vals sort vec))
-        twiddle
-        (fn [chord]
-          (->> (phrase (repeat 5 1/4) (repeat chord))
-               (then (phrase (mapcat repeat [3 4] [1/4 1/2])
-                             (map chord [0 1 2 3 2 1 0]))) 
-               (all :part ::melody)))
-        strum (fn [chord] (phrase (repeat 4 1) (repeat chord)))
-        bonus (-> triad (root -3) (inversion 2) (augment :iii 1/2))
-        ]
-    (->>
-      (reduce #(then %2 %1)
-              [(twiddle (-> triad (inversion 2) (assoc :extra 1) vectorise))
-               (strum (-> triad (root -3)))
-               (twiddle (-> triad (root -4) (assoc :extra -1) vectorise))
-               (phrase (mapcat repeat [2 8] [1 1/4])
-                       (mapcat repeat [2 4 4] [bonus (augment bonus :iii 1/2) bonus]))
-               (twiddle (-> triad (root -4) (inversion 2) (assoc :extra -3) vectorise))
-               (twiddle (-> triad (root -5) (inversion 2) (assoc :extra -4) vectorise))
-               (twiddle (-> triad (root -3) (inversion 1) (assoc :extra -4) vectorise))
-               (strum (-> triad (inversion 2) (root -7)))])
-      (wherever (comp not :part) :part (is ::intro)))))
+(def dux
+  (let [a (phrase
+            (mapcat repeat [8 1 1 4 1] [1 3/2 1/2 1 3/2])
+            [0 1 2 0 1 2 3 1 3 3 2 1 0 -1/2 1])
+        b (phrase
+            [1/2 3/2 1/2 1 1 1 1 3/2 1/2 3/2 1/2 3/2 1/2 1 1 2]
+            [2 3 3 2 1 0 -1 -3 -1 -3 -1 -3 -1 -3 -5 -7])]
+    (->> a (then b))))
 
-(def flourishes 
+(def comes
+  (let [a (phrase [1 1 1 1 1 1 1 1 3/2 1/2 1/2 1/2 1/2 1/2 7/2]
+                  [0 1 2 0 -1 0 1 -1 -2 -3 -4 -2 0 3 1])
+        b (phrase (concat (repeat 25 1/2) [1 1 1/2 1/2 1/2 1/2])
+                  [1 -4 -2 0 3 -4 -2 0 3 -5 -3 -1 2 -5 -3 -1 2 -3 -1 1 -3 1 2 1 -1 0 2 1 0 -1 -3]) ]
+    (->> a (then b))))
+
+(def first-section
+  (let [dux (all :part ::starboard dux)
+        comes (all :part ::port comes)
+        bassline (all :part ::fore bassline)]
+    (->> (with dux (drop 4 comes))
+         (then (with dux comes bassline)))))
+
+
+; Second section
+(def harmony
+  (->> bassline 
+       (where :pitch (from 9))
+       (wherever #(-> % :time (= 12)) :pitch (from 1/2))))
+
+(def line 
   (let [first-flourish (phrase
                          [1/4 1/4 3/2 1 1 9/2]
                          [1 2 3 4 2 1])
@@ -59,169 +57,136 @@
       (phrase [5/2 1/2 1/2 8/2] [4 3 2 4])
       (then first-flourish)
       (then (phrase [5/2 1/4 1/4 9/2] [4 2 3 4]))
-      (then second-flourish) 
-      (all :part ::default))))
+      (then second-flourish))))
 
-(def harmony
-  (->> bassline
-       (where :pitch (from 9))
-       (wherever #(-> % :time (= 12)) :pitch (from 1/2))
-       (all :part ::bass)))
+(def beat 
+  (->> (phrase [3/2 1/2 1/2 3/2] (repeat -21))
+       (times 8)))
 
-(def melody
+(def beatback
+  (->> beat
+       (with (times 4 (phrase (repeat 16 1/2) (cycle [-12 -10]))))))
+
+(def second-section
   (->>
-    (phrase
-      [1/2 1 1/2 2 1/2 1 1/2 2 1/2 1/2 1/2 1 1 1/2 4]
-      [-4 0 0 0 2 4 5 4 4 4 3 2 2 3 4])
+    (with
+      (all :part ::fore bassline)
+      (all :part ::port line)
+      (all :part ::starboard harmony)
+      (all :part ::aft beat))
     (then
-      (phrase
-        [1/2 1/2 1 2 1/2 1/2 1 2]
-        [4 4 3 2 3 4 6 4]))
+      (with
+        (all :part ::fore bassline)
+        (all :part ::port harmony)
+        (all :part ::starboard line) 
+        (all :part ::aft beatback))))) 
+
+
+; Key change
+(def descent
+  (->> (phrase (cycle [4 4 8]) [0 -3 -4 -1 -1.5 -6])
+       (where :pitch lower)))
+
+(def drift
+  (phrase (cycle [3 1 3 1 8])
+          [0 2 1 -1 0 1 -1 0.5 -1.5 1]))
+
+(def bubble
+  (letfn [(riff [z y x] (phrase (repeat 1/2) [z z x z z x z z]))]
+    (->>
+      (riff 4 2 -1)
+      (then (riff 4 1 -1))
+      (then (times 2 (riff 3 0 -2)))
+      (then (riff 3 1 -1))
+      (then (riff 2.5 0.5 -1.5))
+      (then (times 2 (riff 3 1 -1.5))))))
+
+(def funk
+  (->> (phrase [3/2 2/2 1/2 1/2 1/2] (cycle [-14 -14 [-10 -7] [-10 -7] [-10 -7]]))
+       (times 4)))
+
+(def key-change
+  (with
+    (all :part ::fore descent)
+    (all :part ::port drift)
+    (all :part ::starboard bubble)
+    (all :part ::aft (times 2 funk))))
+
+; Third section
+(def dunk
+  (->> funk
+       (with (phrase (repeat 16 1/2) (cycle [nil -3])))
+       (times 4)))
+
+(def circuit
+  (->> (phrase [4 4 8] [-2 -3 0])
+       (times 4)
+       (where :pitch lower)))
+
+(def trace
+  (->> (phrase [4 4 2 2 4]
+               [2 -0.5 2 1 0])
+       (then (phrase [4 4 8]
+               [2 -0.5 2])) 
+       (times 2)))
+
+(def flutter
+  (->> (phrase [4 4 2 2 4]
+               [4 1 4 3 2])
+       (then (phrase [4 4 8]
+                     [4 -0.5 -3]))
+       (times 2)))
+
+(def third-section 
+  (with
+    (all :part ::fore circuit)
+    (all :part ::port trace)
+    (all :part ::starboard flutter)
+    (all :part ::aft dunk)))
+
+; Finale
+(def uplift
+  (phrase (concat (repeat 7 4) [36]) [4 3 4 3 2 4 4 4 4]))
+
+(def peace
+  (->> harmony
+       (but 28 32 (phrase [36] [-5]))))
+
+(def finale 
+  (with
+    (all :part ::fore bassline)
+    (all :part ::port peace)
+    (all :part ::starboard uplift)))
+
+; Arrangement
+(defmethod play-note ::port [{midi :pitch seconds :duration direction :direction}]
+  (some-> midi overtone/midi->hz (corgan seconds :vol 0.7 :pan -1/2 :wet 0.4 :room 0.9 :vibrato 105/60)))
+
+(defmethod play-note ::starboard [{midi :pitch seconds :duration direction :direction}]
+  (some-> midi overtone/midi->hz (corgan seconds :vol 0.7 :pan 1/2 :wet 0.4 :room 0.9 :vibrato 105/15)))
+
+(defmethod play-note ::fore [{midi :pitch seconds :duration direction :direction}]
+  (some-> midi overtone/midi->hz (kraft-bass :vol 3.0 :dur seconds :pan 0 :wet 0.7 :room 0.9)))
+
+(defmethod play-note ::aft [{midi :pitch}] (some-> midi overtone/midi->hz drums/kick2))
+
+
+; Structure
+(def sidhe 
+  (->>
+    (->> first-section
+         (then second-section)
+         (then key-change)
+         (where :pitch (comp C minor)))
     (then
-      (phrase
-        [1/2 3/2 1/2 3/2 1/2 7/2]
-        [2 1 2 1 -1 0]))
-    (after -1/2)
-    (where :part (is ::default))))
-
-(def lead-in
-  (->>
-    (phrase
-      (repeat 1/2)
-      [-3 0 2 1 0])
-    (after -3)
-    (all :part ::default)))
-
-(def progression
-  {:main
-   [(-> ninth (root 0))
-    (-> seventh (root 4))
-    (-> seventh (root 3))
-    (-> triad (root 4) (augment :iii 1/2))
-    (-> seventh (root 3))
-    (-> triad (root 2))
-    (-> seventh (root 4))
-    (-> seventh (root 0))]
-   :transition
-   [(-> triad (inversion 2))
-    (-> triad (root -3))
-    (-> triad (root -4))]
-   :emphasis
-   [(-> triad (root -1) (inversion 2))
-    (-> triad (root -3/2) (inversion 2))
-    (-> triad (root 1) (inversion 1) (augment :v 1/2))]})
-
-(def chords
-  (->>
-    (:main progression)
-    (phrase (repeat 4))
-    (where :pitch raise)
-    (all :part ::chords)))
-
-(def fall-down
-  (let [beat (->>
-               (rhythm (repeat 32 1/2))
-               (having :drum (cycle [:kick :click :click
-                                     :kick :click :click
-                                     :kick :click]))
-               (where :part (is ::beat))
-               (after 16))]
-    (->>
-      (phrase (cycle [4 4 8])
-              [0 -3 -4 -1 -3/2 1])
-      (where :pitch lower)
-      (where :part (is ::bass))
-      (with (->>
-              (phrase [1 3 1 3 1 7 1 3 1 3 1 8]
-                      (interleave [0 3 -1 0 -1 -3/2]
-                                  (concat (:transition progression) (:emphasis progression))))
-              (after -1)))
-      (where :part (is ::default))
-      (with beat) 
-      )))
-
-(def emphasis
-  (let [melody
-        (phrase [3 1/2 1/2 2 2 2 2 4] [3 4 3 2.5 0.5 5.5 4 3])
-        beat (->> (cycle [1/2 7 1/2]) rhythm (all :drum :kick)
-                  (all :part ::beat)
-                  (take 23))]
-    (->> (:emphasis progression)
-         (phrase [4 4 8])
-         (times 2)
-         (where :part (is ::chords))
-         (with (->> melody (then (->> melody (drop-last 3) (then (phrase [4] [1]))))
-                    (all :part ::default)))
-         (times 2)
-         (with beat))))
-
-(def breakdown
-  (let [dux-a
-        (phrase
-          (mapcat repeat [8 1 1 4 1] [1 3/2 1/2 1 3/2])
-          [0 1 2 0 1 2 3 1 3 3 2 1 0 -1/2 1])
-        dux-b
-        (phrase
-          [1/2 3/2 1/2 1 1 1 1 3/2 1/2 3/2 1/2 3/2 1/2 1 1 2]
-          [2 3 3 2 1 0 -1 -3 -1 -3 -1 -3 -1 -3 -5 -7])
-        comes-a
-        (after 4
-               (phrase
-                 [1 1 1 1 3/2 1/2 1/2 1/2 1/2 1/2 7/2]
-                 [-1 0 1 -1 -2 -3 -4 -2 0 3 1]))
-        comes-b
-        (phrase (concat (repeat 25 1/2) [1 1 1/2 1/2 1/2 1/2])
-                [1 -4 -2 0 3 -4 -2 0 3 -5 -3 -1 2 -5 -3 -1 2 -3 -1 1 -3 1 2 1 -1 0 2 1 0 -1 -3])
-        ]
-    (->>
-      dux-a
-      (then dux-b)
-      (with (->> comes-a (then comes-b)) )
-      (times 2)
-      (with (after 32 bassline))
-      (all :part ::melody))))
-
-(def kit {:kick drums/kick2 
-          :tick drums/closed-hat,
-          :tock drums/open-hat
-          :click click})
-
-(defmethod play-note ::beat [note] ((-> note :drum kit)))
-(defmethod play-note ::default [note] (pick 0.99 0.1 note))
-(defmethod play-note ::intro [{midi :pitch seconds :duration}]
-  (organ (overtone/midi->hz midi) seconds 0.7))
-(defmethod play-note ::bass [note]
-  (-> note (assoc :part ::default) play-note))
-(defmethod play-note ::melody [note]
-  (-> note (assoc :part ::default) play-note))
-(defmethod play-note ::chords [{midi :pitch seconds :duration}]
-  (organ (overtone/midi->hz midi) seconds 0.5))
-
-(def piece 
-  (->>
-    breakdown
-    (then (with bassline harmony flourishes))
-    (then (with beat bassline harmony flourishes chords))
-    (then (with bassline harmony lead-in melody))
-    (then (with bassline harmony melody beat))
-    (then fall-down)
-    (then emphasis)
-    (where :pitch (comp C minor))
+      (->> third-section
+           (then first-section)
+           (then second-section)
+           (then key-change)
+           (where :pitch (comp D minor))))
+    (then
+      (->> third-section
+           (then first-section)
+           (then finale)
+           (where :pitch (comp E minor))))
     (in-time (bpm 105))))
-
-(def sidhe
-  (mapthen
-    (fn [round] (wherever :pitch, :pitch #(+ % (* 2 round)) piece))
-    (range 0 2)))
-
-(def sidhe-sparse
-  (->> 
-    (with intro bassline)
-    (then (with intro bassline beat))
-    (then fall-down)
-    (then emphasis)
-    (where :pitch (comp B flat minor))
-    (then (->> breakdown (then (phrase [64] [0]))
-               (where :pitch (comp C minor))
-               (where :part (is ::melody))))
-    (in-time (bpm 80))))
