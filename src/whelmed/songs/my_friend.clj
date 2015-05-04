8bcba0f4b2a3f1878bf54ec4ddc42150e7d543f6
Chris Ford <christophertford@gmail.com>
2015-05-04 20:41:31 +0100

    Use absolute keywords to avoid clashes.

--- src/whelmed/songs/my_friend.clj
+++ src/whelmed/songs/my_friend.clj
@@ -12,20 +12,20 @@
 (def the-key (comp temperament/equal scale/F scale/major))
 
 ; Arrangement
-(defmethod live/play-note :bass
+(defmethod live/play-note ::bass
   [{hertz :pitch seconds :duration}]
   (some-> hertz (bass seconds :res (the-key 14) :pan -1/3 :wet 0.7 :room 0.1)))
 
-(defmethod live/play-note :accompaniment
+(defmethod live/play-note ::accompaniment
   [{hertz :pitch seconds :duration}]
   (some-> hertz (organic seconds :wet 0.8 :pan 1/3))
   (some-> hertz (* 1.0001) (organic seconds :wet 0.9 :pan -1/3)))
 
-(defmethod live/play-note :melody
+(defmethod live/play-note ::melody
   [{hertz :pitch seconds :duration}]
   (some-> hertz (sing seconds :wet 0.3)))
 
-(defmethod live/play-note :beat
+(defmethod live/play-note ::beat
   [{hertz :pitch drum :drum}]
   (some-> hertz (drum :volume 0.5)))
 
@@ -53,20 +53,20 @@
       (then walk)
       (filter :pitch)
       (where :pitch (comp scale/lower scale/lower))
-      (all :part :bass))))
+      (all :part ::bass))))
 
 (def flatline
   (->>
     (phrase (cycle [4 1/2 1/2 3]) (repeat 0))
     (take-while #(-> % :time (< 32)))
     (where :pitch (comp scale/lower scale/lower))
-    (all :part :bass)))
+    (all :part ::bass)))
 
 (def flock
   (->>
     (phrase (repeat 2) (mapcat repeat (repeat 2) chords))
     (times 2)
-    (all :part :accompaniment)))
+    (all :part ::accompaniment)))
 
 (def accompaniment
   (->>
@@ -75,7 +75,7 @@
     (then (phrase [3/3 1/3 8/3] [(chords 2) (chords 2) nil]))
     (then (phrase [4] [(chords 1)]))
     (filter :pitch)
-    (all :part :accompaniment)
+    (all :part ::accompaniment)
     (times 2)))
 
 (def core-med
@@ -85,7 +85,7 @@
     (after -1)
     (then (phrase [15 1 7 1 7 1] [7 8 9 8 7 8]))
     (where :pitch scale/raise)
-    (all :part :melody)))
+    (all :part ::melody)))
 
 (def my-friend
   (let [i-know
@@ -104,7 +104,7 @@
          (then anyone-else)
          (times 2)
          (where :pitch scale/raise)
-         (all :part :melody))))
+         (all :part ::melody))))
 
 (def ba-da 
   (->> (phrase [1 1 1 1/2 1 1/2 1/2 1/2 1 1/2 1/2]
@@ -112,7 +112,7 @@
        (canon/canon (canon/interval -7))
        (times 8)
        (where :pitch scale/raise)
-       (all :part :melody)))
+       (all :part ::melody)))
 
 (def twiddle
   (let [twid (fn [a b c] (phrase [1/2 1/2 1/2 3/2 1/2 1/2] [b a b c b a]))]
@@ -120,14 +120,14 @@
          (then (twid -2 1 2)) 
          (where :pitch scale/raise)
          (after 24)
-         (all :part :accompaniment))))
+         (all :part ::accompaniment))))
 
 (def beat1
   (let [k (->> (phrase [1 1 2/3 1/3 1/3 1/3 1/3 1 1 1 1] (repeat -14))
                        (all :drum kluck))
         t (->> (phrase [2 2 2 1] (repeat 14)) (all :drum tip))]
     (->> (with k t)
-         (all :part :beat)
+         (all :part ::beat)
          (times 4))))
 
 (def beat2
@@ -138,7 +138,7 @@
                (all :drum tip)
                (after 1/2))]
     (->> (with k t)
-         (all :part :beat)
+         (all :part ::beat)
          (times 4))))
 
 ; Track
@@ -161,7 +161,7 @@
 (def bridge
   (let [bass (->> (phrase (repeat 4) (concat (range 0 7) [8])) 
                   (where :pitch (comp scale/lower scale/lower))
-                  (all :part :bass))
+                  (all :part ::bass))
         arpeggs (->> (phrase (cycle [2 1 1
                                      1 1 1 1])
                              [2 0 -3
@@ -169,7 +169,7 @@
                               4 2 -1
                               -2 0 1 0])
                      (times 2)
-                     (all :part :accompaniment))]
+                     (all :part ::accompaniment))]
     (->> (with bass arpeggs core-med)
          (then (with flatline flock arpeggs core-med)))))
 
