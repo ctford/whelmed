5e79bd4c56ede6b1d15dd40e3f2065a63944c435
Chris Ford <christophertford@gmail.com>
2015-05-07 16:07:54 +0100

    Add lyrics to Sidhe, though the final verse is missing.

--- src/whelmed/songs/sidhe.clj
+++ src/whelmed/songs/sidhe.clj
@@ -174,6 +174,52 @@
 
 ; Structure
 (def sidhe 
+
+  "A young man once a wand'ring went,
+  He deemed he'd come of age,
+  He severed every earthly tie,
+  And curses his home a cage,
+  
+  His tracks, which trailed the setting sun,
+  A thousand furlongs spanned,
+  Pressed in clay and marked in mud,
+  And printed on the sand,
+  
+  At last he reached the border peaks,
+  With which the world is crowned,
+  He sought a tarn to bathe his feet,
+  And such a tarn he found,
+  
+  'The mountain cups his gnarléd hand,
+  To pool the liquid sky,
+  Some vain goddess's looking glass,
+  Let fall and then let lie',
+  
+  So read his later vain attempts,
+  To capture with his pen,
+  A crystel well of wilderness,
+  Transcending mortal ken,
+  
+  Yet as he stood in reverie,
+  The nightfall stole around,
+  Stars unsheathed above his head,
+  And mist rose from the ground,
+  
+  And then, reflected in the tarn,
+  The starlight coalesced,
+  Our young man's ideal fantasy,
+  Of loveliness, expressed,
+
+  Her skin shone white as angel's robes, 
+  Her hair swirled black as pitch, 
+  Two shards of ice comprised her eyes,
+  Two drops of blood - her lips.
+  
+  Yet as he bent to better see,
+  His touch disturbed the water,
+  Her face dissolved and left him only,  
+  Faint and distant laughter."
+
   (->>
     (->> first-section
          (then second-section)
