346dd7c3e06c96e0a990b9bb3e4011c38d3b49bd
Chris Ford <christophertford@gmail.com>
2015-05-09 20:07:42 +0100

    Add JVM option that supposedly helps performance.

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Whelmed. Not overwhelmed. Not underwhelmed. Just whelmed."
   :url "http://github.com/ctford/whelmed"
   :main ^{:skip-aot true} whelmed.play
+  :jvm-opts ^:replace  []
   :dependencies	[
     [org.clojure/clojure "1.5.1"]
     [overtone "0.9.1"]
