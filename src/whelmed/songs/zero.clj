4bec2b9c8d8e8e298c2556ada5d8edcd48f6fcd0
Chris Ford <christophertford@gmail.com>
2015-06-29 21:39:56 +0100

    Make sure the part can't clash.

--- src/whelmed/songs/zero.clj
+++ src/whelmed/songs/zero.clj
@@ -1,11 +1,11 @@
 (ns whelmed.songs.zero
   (:require [leipzig.live :as live]))
 
-(defmethod live/play-note :piano [_])
+(defmethod live/play-note ::piano [_])
 
 (def zero
   "A cover version of John Cage's 273, but accurate to two more decimal places."
-  [{:time 0 :duration 273.15 :part :piano}])
+  [{:time 0 :duration 273.15 :part ::piano}])
 
 (comment
   (->> zero live/play)
